6585e37c9a0c8c27646325326e4372c4002d7ada
saivitasoft <saikumarvita@gmail.com>
2023-11-05 11:34:58 +0530

    component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
